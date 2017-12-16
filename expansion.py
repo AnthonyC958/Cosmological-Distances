75c5109e0547032082dfb59cf7d47ddcdeaabe8d
Anthony Carr <anthonyc958@gmail.com>
2017-12-16 15:59:53 +1000

    [WIP] Almost finished total_perpendicular distance (added new plots and made distances independent of h).

--- expansion.py
+++ expansion.py
@@ -13,12 +13,13 @@ def get_h_inv(z_val, matter_dp, lambda_dp):
      ol -- dark energy density parameter
     """
     curvature_dp = 1.0 - matter_dp - lambda_dp
-    h = np.sqrt(curvature_dp * (1 + z_val) ** 2 + matter_dp * (1 + z_val) ** 3 + lambda_dp)
-    return 1. / h
+    H = np.sqrt(curvature_dp * (1 + z_val) ** 2 + matter_dp * (1 + z_val) ** 3 + lambda_dp)
+    return 1. / H
 
 
 def parallel(matter_dp, lambda_dp, zs_array):
-    H0 = 70000  # km/s/Gpc
+    H0 = 70000  # km/s/Gpc assuming h = 0.70
+    H0 = 100000  # in terms of h
     c = 2.998e5  # km/s
     h_invs = vecGet_h_inv(zs_array, matter_dp, lambda_dp)
     curvature_dp = 1.0 - matter_dp - lambda_dp
@@ -36,6 +37,7 @@ def parallel(matter_dp, lambda_dp, zs_array):
 
 def numerical_check(matter_dp, lambda_dp, zs_array, q0):
     H0 = 70000
+    H0 = 100000
     c = 2.998e5
     test_dist = parallel(matter_dp, lambda_dp, zs_array)
     analytical_dist = -c / H0 * 2 * (1 / np.sqrt(zs_array + 1) - 1)  # om = 1, ol = 0
@@ -47,9 +49,9 @@ def numerical_check(matter_dp, lambda_dp, zs_array, q0):
     # ax.plot(zs_array, Liske_dist, label="Liske")  # Liske distance doesn't match
     ax.legend(loc="upper left", frameon=False, bbox_to_anchor=(0.7, 0.8))
     ax.set_xlabel("$z$", fontsize=16)
-    ax.set_ylabel("$R_0\chi$ (Gpc)", fontsize=16)
+    ax.set_ylabel("$R_0\chi$ (h$^{-1}$Gpc)", fontsize=16)
     ax.set_title("Parallel Check", fontsize=20)
-    # fig.savefig("analytical check.pdf", bbox_inches="tight")
+    fig.savefig("analytical check.pdf", bbox_inches="tight")
     plt.show()
 
 
@@ -74,14 +76,14 @@ def plot_parallel(zs_array, dist, curvature):
     ax.legend(loc="upper left", frameon=False, bbox_to_anchor=(0.7, 0.8))
     plt.figtext(0.85, 0.6, "$\Omega_\Lambda=%0.2f$" % ol, ha='right', va='bottom', weight='roman', size='large')
     ax.set_xlabel("$z$", fontsize=16)
-    ax.set_ylabel("$R_0\chi$ (Gpc)", fontsize=16)
+    ax.set_ylabel("$R_0\chi$ (h$^{-1}$Gpc)", fontsize=16)
     if curvature == 0:
         ax.set_title("Parallel Distance (Flat Space)", fontsize=20)
     elif curvature < 0:
         ax.set_title("Parallel Distance (Negative Curved Space)", fontsize=20)
     else:
         ax.set_title("Parallel Distance (Positive Curved Space)", fontsize=20)
-    # fig.savefig("parallel.pdf", bbox_inches="tight")
+    fig.savefig("parallel.pdf", bbox_inches="tight")
     plt.show()
 
 
@@ -90,7 +92,7 @@ def plot_perp_thet(z_contours, thetas_array, dist, curvature):
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     ax.set_xlabel("$\\theta$", fontsize=16)
-    ax.set_ylabel("$R\chi$ (Gpc)", fontsize=16)
+    ax.set_ylabel("$R\chi$ (h$^{-1}$Gpc)", fontsize=16)
     for num, contour in enumerate(z_contours, start=0):
         ax.plot(thetas_array, dist[num], label=f"$z={contour}$", color=colours[num])
     ax.legend(loc="upper left", frameon=False)  # ,bbox_to_anchor=(0, 0.5))
@@ -101,7 +103,7 @@ def plot_perp_thet(z_contours, thetas_array, dist, curvature):
     else:
         ax.set_title("$\\theta$ Perpendicular Distance (Positive Curved Space)", fontsize=20)
     plt.show()
-    # fig.savefig("theta.pdf", bbox_inches="tight")
+    fig.savefig("theta.pdf", bbox_inches="tight")
 
 
 def plot_perp_phi(z_contours, phis_array, dists, curvature):
@@ -109,7 +111,7 @@ def plot_perp_phi(z_contours, phis_array, dists, curvature):
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     ax.set_xlabel("$\\phi$", fontsize=16)
-    ax.set_ylabel("$R\chi\sin\\theta$ (Gpc)", fontsize=16)
+    ax.set_ylabel("$R\chi\sin\\theta$ (h$^{-1}$Gpc)", fontsize=16)
     for num, contour in enumerate(z_contours, start=0):
         ax.plot(phis_array, dists[0][num], label=f"$z={contour}$", color=colours[num])
     ax.legend(loc="upper left", frameon=False)  # ,bbox_to_anchor=(0, 0.5))
@@ -122,7 +124,7 @@ def plot_perp_phi(z_contours, phis_array, dists, curvature):
     else:
         ax.set_title("$\\phi$ Perpendicular Distance (Positive Curved Space)", fontsize=20)
     plt.show()
-    # fig.savefig("phi.pdf", bbox_inches="tight")
+    fig.savefig("phi.pdf", bbox_inches="tight")
 
 
 def get_line_integrand(lambda_val, theta1, theta2, phi1, phi2):
@@ -136,7 +138,7 @@ def get_line_integrand(lambda_val, theta1, theta2, phi1, phi2):
     theta = theta2 - theta1
     phi = phi2 - phi1
     theta_l = theta1 + lambda_val * theta
-    integrand = np.sqrt(theta**2 + np.sin(theta_l)**2 * phi**2)  # from angular part of FRW metric,
+    integrand = np.sqrt(theta ** 2 + np.sin(theta_l) ** 2 * phi ** 2)  # from angular part of FRW metric,
     # or metric of a sphere, parametrised.
     return integrand
 
@@ -156,7 +158,7 @@ def calculate_line_integral(phi_val, radius, theta1, theta2, phi1):
     return integral[-1]
 
 
-def total_perp(matter_dp, lambda_dp, zs_array, z_contour, phis_array, theta1, theta2, phi1):
+def total_perp(matter_dp, lambda_dp, zs_array, z_contour, phis_array, theta1, theta2):
     """Calculates the total perpendicular distance in three ways.
     The ways are: approximation from pythagorean approximation using separate distance, analytical solution to arc
     length of great circle connecting the points (only used a check for the actual integration method), and the method
@@ -168,18 +170,18 @@ def total_perp(matter_dp, lambda_dp, zs_array, z_contour, phis_array, theta1, th
      z_contour -- the redshift giving the radius of the sphere
      phis_array -- array of phi values to find distances as a function of phi
     """
-
+    phi1 = 0
     # Find approximate distance
     dist2 = perp_thet(matter_dp, lambda_dp, zs_array, z_contour, theta1)  # distance along theta to 1st pt.
     dist1 = perp_thet(matter_dp, lambda_dp, zs_array, z_contour, theta2)  # distance to theta co-ordinate of 2nd pt.
     theta_dist = dist2 - dist1
     phi_dist = perp_phi(matter_dp, lambda_dp, zs_array, z_contour, theta2, phis_array)  # array of phi distances, so
     # D_perp is a function of phi
-    dist_perp = np.sqrt(theta_dist**2 + phi_dist**2)
+    dist_perp = np.sqrt(theta_dist ** 2 + phi_dist ** 2)
 
     # # Find analytical value
     parallel_dists = parallel(matter_dp, lambda_dp, zs_array)  # parallel distances to z_max
-    radius = parallel_dists[np.argmin(np.abs(zs_array - z_contour))]   # select only the distance corresponding to the
+    radius = parallel_dists[np.argmin(np.abs(zs_array - z_contour))]  # select only the distance corresponding to the
     # # radius of the sphere
     # analytical = radius * np.arccos(np.sin(theta1) * np.sin(theta2) * np.cos(phis_array) + np.cos(theta1)
     #                                 * np.cos(theta2))
@@ -200,8 +202,8 @@ if __name__ == "__main__":
     z_nstep = 101
     z_arr = np.linspace(z_lo, z_hi, z_nstep)
 
-    om = 1.0
-    ol = 0.0
+    om = 0.3
+    ol = 0.7
     ok = 1.0 - om - ol
 
     phi_arr = np.linspace(0, 2 * np.pi, 101)
@@ -217,7 +219,8 @@ if __name__ == "__main__":
         dist_thet[i][:] = perp_thet(om, ol, z_arr, z, theta_arr)
 
     # Calculate phi perpendicular distance with two sample thetas
-    thetas = np.array([0.01745, 2 * 0.01745])  # 1 and 2 degrees for phi perpendicular distance
+    deg = 0.01745  # 1 degree in radians.
+    thetas = np.array([deg, 2 * deg])  # 1 and 2 degrees for phi perpendicular distance
     dist_phi = np.arange(zs.size * z_arr.size * thetas.size, dtype=np.float64).reshape(thetas.size, zs.size, z_arr.size)
     for i, angle in enumerate(thetas):
         for j, z in enumerate(zs, start=0):
@@ -225,26 +228,43 @@ if __name__ == "__main__":
 
     # Temporary terrible code to get everything working
     # Calculate the three total perpendicular distances
-    x, w, diff = total_perp(om, ol, z_arr, 10, phi_arr, np.pi/6, np.pi/5, 0)
+    z_radius = 1
+    theta_starts = np.array([0, 0, 15 * deg, 15 * deg, 30 * deg, 30 * deg])
+    theta_ends = np.array([5 * deg, 10 * deg, 20 * deg, 25 * deg, 35 * deg, 40 * deg])
+    dist_perps = np.arange(theta_starts.size * phi_arr.size, dtype=np.float64).reshape(
+        theta_starts.size, phi_arr.size)
+    theory_perps = np.arange(theta_starts.size * phi_arr.size, dtype=np.float64).reshape(
+        theta_starts.size, phi_arr.size)
+    dist_diffs = np.arange(theta_starts.size * phi_arr.size, dtype=np.float64).reshape(
+        theta_starts.size, phi_arr.size)
     fig = plt.figure()
     ax = plt.subplot2grid((4, 1), (0, 0), rowspan=3)
-    ax.plot(phi_arr[:51], x[:51], label="Approximation")  # only makes sense to plot 0 to pi
-    ax.plot(phi_arr[:51], w[:51], label="Numerical")
+    ax.set_ylabel("$D_\perp$ (h$^{-1}$Gpc)", fontsize=16)
     ax.set_title("Total Parallel Distance", fontsize=20)
-    ax.legend(loc="upper left", frameon=False, bbox_to_anchor=(0.1, 0.8))
-    ax.set_ylabel("$D_\perp$ (Gpc)", fontsize=16)
-    ax.tick_params(labelbottom=False)
-
     ax2 = plt.subplot2grid((4, 1), (3, 0))
-    ax2.plot(phi_arr[:51], diff[:51], label="Difference", color="r")
-    ax2.plot(phi_arr[:51], np.linspace(0, 0, 51), linestyle="--", color="k")
     ax2.set_xlabel("$\phi$", fontsize=16)
-    ax2.set_ylabel("Diff. (Gpc)")
-    # fig.savefig("total_perp.pdf", bbox_inches="tight")
+    ax2.set_ylabel("Diff. (h$^{-1}$Gpc)")
+    ax.tick_params(labelbottom=False)
+    ax2.plot(phi_arr[:51], np.linspace(0, 0, 51), linestyle="--", color="k")
+    colours = matplotlib.cm.rainbow(np.linspace(0, 1, theta_ends.size))
+    for l, end in enumerate(theta_ends, start=0):
+        dist_perps[l], theory_perps[l], dist_diffs[l] = total_perp(om, ol, z_arr, z_radius, phi_arr,
+                                                                   theta_starts[l], theta_ends[l])
+        ax.plot(phi_arr[:51], dist_perps[l][:51], label=f"$\Delta\\theta$ = {int(np.ceil(theta_starts[l]/deg))}$^\circ$"
+                                                        + f" - {int(theta_ends[l]/deg)}$^\circ$", color=colours[l])
+        ax.legend(loc="upper left", frameon=False, bbox_to_anchor=(0.05, 0.9))
+    for l, end in enumerate(theta_ends, start=0):
+        dist_perps[l], theory_perps[l], dist_diffs[l] = total_perp(om, ol, z_arr, z_radius, phi_arr,
+                                                                   theta_starts[l], theta_ends[l])
+        ax.plot(phi_arr[:51], theory_perps[l][:51], label="Numerical", linestyle="--", color=colours[l])
+
+        ax2.plot(phi_arr[:51], dist_diffs[l][:51], label="Difference", color=colours[l])
+
     plt.show()
+    fig.savefig("total_perp.pdf", bbox_inches="tight")
 
     # No need for these right now
     # numerical_check(om, ol, z_arr, 0.5)
-    # plot_parallel(z_arr, dist_para, ok)
-    # plot_perp_thet(zs, theta_arr, dist_thet, ok)
-    # plot_perp_phi(zs, phi_arr, dist_phi, ok)
+    plot_parallel(z_arr, dist_para, ok)
+    plot_perp_thet(zs, theta_arr, dist_thet, ok)
+    plot_perp_phi(zs, phi_arr, dist_phi, ok)
